38009f18587f81e94b3206887c3eb4b059ce8344
CodeDifferentlyGuides <65664324+CodeDifferentlyGuides@users.noreply.github.com>
2020-05-25 02:25:27 -0400

    Created ResultTest.java

---
+++ test/ResultTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void contructResultTest(){
+        String input = "08 05 2015"; 
+        String expected = "WEDNESDAY";
+        String actual = "*insert main function call*";
+
+        Assert.assertEquals(expected, actual);
+    }
+}
